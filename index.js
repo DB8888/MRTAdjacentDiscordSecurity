3df775eb7163cd5a79bdc35d3fff777c85347975
DB8888 <67452089+DB8888@users.noreply.github.com>
2020-09-28 21:29:15 +0100

    update invite link + error prevention

--- index.js
+++ index.js
@@ -33,15 +33,22 @@ bot.on('guildMemberAdd', (auxiliaryMember) => {
 
 
     if (auxiliaryMember.guild === MRTServer) return 0; //ignore member being added to mrt discord
-    if (!MRTServer.member(auxiliaryMember.user.id)) {
+
+
+    if (!MRTServer.member(auxiliaryMember.user.id)) {//if user isnt on the mrt discord
         let owner = bot.users.cache.get(auxiliaryMember.guild.ownerID);
         let tag = auxiliaryMember.user.tag;
         let guild = auxiliaryMember.guild.name;
+
+        if(!auxiliaryMember.bannable) return owner.send('A suspicious user joined your Discord, but I was unable to ban them. Please ensure that I have the appropriate permissions and that my role is above the default role.');
+
         auxiliaryMember.send(`You don't seem to be a member of the MRT Discord. Ask a staff member in-game for the link and once you have joined, ask ${owner} (${owner.tag}) to be unbanned.`).then(() => {
             auxiliaryMember.ban({ reason: 'Not on the MRT Discord' }).then(() => {
                 loggingChannel.send(`Banned ${tag} from ${guild}.`);
                 if (auxiliaryMember.guild.systemChannel) {
-                    auxiliaryMember.guild.systemChannel.send(`:white_check_mark: banned ${tag}`)
+                    auxiliaryMember.guild.systemChannel.send(`:white_check_mark: banned ${tag}`).catch(err =>{
+                        console.log(err);
+                    })
                 }
             })
 
